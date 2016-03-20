d7165ec6550a2ad897c659c3e68cbadacb2a4bef
Jeff Flowers <flowers.jeff@gmail.com>
2016-03-19 18:27:59 -0700

    getting the LCD to work

--- bitcoinDisplay.py
+++ bitcoinDisplay.py
@@ -11,7 +11,7 @@ import time
 #per the user
 #
 minBtc = 0.01
-waitTimeSec = 120
+waitTimeSec = 5
 baseBitcoin = 'https://btc.blockr.io/api/v1/address/info/'
 addrBitcoin = '198aMn6ZYAczwrE5NvNTUMyJ5qkfy4g3Hi'
 
@@ -26,22 +26,44 @@ from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 lcd = Adafruit_CharLCDPlate()
 
 #Read the bitcoin addresses from external file
-#first determine how many addresses there are
-#Important to end the file on an Address
-#
-totalNumAddr=0
-#Not the most eff, but gets the job done
+#and input into an array
 #
 with open('addressBitcoin.txt') as f:
-	totalNumAddr = sum(1 for _ in f)
-
-
-#Testing to see if I can just get one address
+	arrayAddr = []
+	for line in f:
+		arrayAddr.append(line)
+#Number of elements the array
 #
-btcAPI = urllib2.urlopen(baseBitcoin + addrBitcoin)
-response = btcAPI.read()
-responseDictionary = json.loads(response)
-if responseDictionary['status'] == 'success':
-	bitcoinBalance = responseDictionary['data']['balance']
-else:
-	bitcoinBalance = 'Err - Check Internet'
+arrayAddrLen = len(arrayAddr) + 1
+i=0
+while True:
+	if i > arrayAddrLen:
+		i = 0
+	else:
+		lcd.clear()
+		lcd.backlight(lcd.GREEN)
+		lcd.message('Getting New\nData from Blockr.io')
+	
+		addrBitcoin = arrayAddr[i]
+		btcAPI = urllib2.urlopen(baseBitcoin + addrBitcoin)
+		response = btcAPI.read()
+		responseDictionary = json.loads(response)
+	
+		if responseDictionary['status'] == 'success':
+			bitcoinBalance = responseDictionary['data']['balance']
+			if bitcoinBalance < minBtc:
+				lcd.backlight(lcd.RED)
+				lcd.message(addrBitcoin + '\nCheck MT')
+			else:
+				lcd.message(addrBitcoin + '\nContains:' + str(bitcoinBalance))
+				print addrBitcoin
+				print str(bitcoinBalance)
+				print i
+				time.sleep(waitTimeSec)
+				i = i + 1
+		else:
+			bitcoinBalance = 'Err - Check Internet'
+			lcd.backlight(lcd.RED)
+			lcd.message(bitcoinBalance)
+			time.sleep(waitTimeSec)
+
